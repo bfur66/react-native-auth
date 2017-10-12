0afe1ee8e938c1a4b9bc494e670f960232973e4a
Brent Furlong <brentfurlong66@gmail.com>
2017-10-12 14:28:18 -0230

    added firebase email/password auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import firebase from 'firebase'
-import { Header } from './components/common'
+import firebase from 'firebase';
+import { Header, Button, CardSection, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
   
   componentWillMount() {
     firebase.initializeApp({
@@ -13,16 +15,52 @@ class App extends Component {
       projectId: "authentication-9e16e",
       storageBucket: "authentication-9e16e.appspot.com",
       messagingSenderId: "914877819485"
-    })
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+        <CardSection>
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        </CardSection>);
+
+      case false:
+          return <LoginForm />;
+
+      default:
+          return ( 
+          <View style={styles.spinnerStyle}>
+            <Spinner size="large" />
+          </View>
+        );
+    }
   }
   
   render() {
     return (
       <View>
         <Header headerText ="Authentication" />
-        <Text>An App!</Text>
+        {this.renderContent()}
       </View>
     );
+  }
+}
+
+const styles = {
+  spinnerStyle: {
+    paddingTop: 40
   }
 }
 
