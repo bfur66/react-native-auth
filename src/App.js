c576e368dc5d0cd27300e0bba08817a94ca8b0ec
Brent Furlong <brentfurlong66@gmail.com>
2017-10-16 17:13:14 -0230

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ class App extends Component {
   
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyCRxn6lT4U-PtNLdoPO-v4qlLsXVqmhE6U",
-      authDomain: "authentication-9e16e.firebaseapp.com",
-      databaseURL: "https://authentication-9e16e.firebaseio.com",
-      projectId: "authentication-9e16e",
-      storageBucket: "authentication-9e16e.appspot.com",
-      messagingSenderId: "914877819485"
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: ""
     });
 
     firebase.auth().onAuthStateChanged((user) => {
